9c580fb228627dd2d18976e6e53c76bbb64b7aba
galvanic-mgt <ryue@galvanic.biz>
2026-07-01 16:49:28 +0800

    Update returnLocationOptions dates to 26/8/2026

--- src/pre_event_apply_config.js
+++ src/pre_event_apply_config.js
@@ -21,8 +21,8 @@ window.PRE_EVENT_APPLY_CONFIG = {
     { value: "2026-08-25T22:00", label: "25/8/2026 22:00" }
   ],
   returnLocationOptions: [
-    { value: "tsuen_wan_west_20260825_1800", time: "2026-08-25T18:00", label: "荃灣西 25/8/2026 18:00" },
-    { value: "tsuen_wan_west_20260825_2200", time: "2026-08-25T22:00", label: "荃灣西 25/8/2026 22:00" },
+    { value: "tsuen_wan_west_20260825_1800", time: "2026-08-25T18:00", label: "荃灣西 26/8/2026 18:00" },
+    { value: "tsuen_wan_west_20260825_2200", time: "2026-08-25T22:00", label: "荃灣西 26/8/2026 22:00" },
     { value: "own_return", time: "", label: "自行安排" }
   ],
   accommodationOptions: [
