c14a33a320e1335ed527de035c1ee61e6367e954
galvanic-mgt <ryue@galvanic.biz>
2026-07-01 16:07:55 +0800

    Text change option +240 to +180

---
+++ pre_event_apply_config.js
@@ -0,0 +1,37 @@
+window.PRE_EVENT_APPLY_CONFIG = {
+  firebaseBase: "https://eva-lucky-draw-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  registrationDeadline: "",
+  revealFrom: "",
+  revealDaysBeforeEvent: 7,
+  transportOptions: [
+    { value: "self_arrangement", label: "自行到場 Self-arrangement" },
+    { value: "shuttle_bus", label: "旅遊巴 Shuttle Bus" }
+  ],
+  goTimeOptions: [
+    { value: "2026-08-25T07:30", label: "25/8/2026 07:30" }
+  ],
+  pickupLocationOptions: [
+    { value: "prince_edward_20260825_0730", time: "2026-08-25T07:30", label: "太子 25/8/2026 07:30" },
+    { value: "tsuen_wan_west_20260825_0730", time: "2026-08-25T07:30", label: "荃灣西 25/8/2026 07:30" },
+    { value: "own_departure", time: "", label: "自行安排" }
+  ],
+  fixedReturnTime: "",
+  returnTimeOptions: [
+    { value: "2026-08-25T18:00", label: "25/8/2026 18:00" },
+    { value: "2026-08-25T22:00", label: "25/8/2026 22:00" }
+  ],
+  returnLocationOptions: [
+    { value: "tsuen_wan_west_20260825_1800", time: "2026-08-25T18:00", label: "荃灣西 25/8/2026 18:00" },
+    { value: "tsuen_wan_west_20260825_2200", time: "2026-08-25T22:00", label: "荃灣西 25/8/2026 22:00" },
+    { value: "own_return", time: "", label: "自行安排" }
+  ],
+  accommodationOptions: [
+    { value: "single_room_240", label: "單人房 +$180" },
+    { value: "twin_room", label: "雙人房" },
+    { value: "no_accommodation", label: "不住宿" }
+  ],
+  mealOptions: [
+    { value: "non_vegetarian", label: "正常餐食 Non-Vegetarian" },
+    { value: "vegetarian", label: "素食者 Vegetarian" }
+  ]
+};
